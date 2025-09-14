19731f1e7bfd395754f43c62a31c1cd412bfd015
andersonhavah <havahanderson@gmail.com>
2025-09-14 00:10:54 +0000

    Refactored server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
- 
-router.get('/', (req, res) => {
-  res.send('Hello World, This is Anderson\'s router');
-});
- 
-app.use('/', router);
-
+const routes = require('./routes');
 const port = 3000;
+
+app.use('/', routes);
+
  
 app.listen(process.env.PORT || port, () => {
     console.log('Web Server is listening at port ' + (process.env.PORT || port));
